131d886cd6231975cc7a155d4314c0a927d09392
Aaron Ayerdis Espinoza <aaronaej7@gmail.com>
2023-03-08 17:28:51 -0600

    Initial commit

---
+++ src/FileCabinet/SuiteScripts/ethos/CustomerDeposit/suitescripts_customer_deposit/ethos_suitelet_customer_deposit.js
@@ -0,0 +1,28 @@
+/**
+ * @NApiVersion 2.1
+ * @NScriptType Suitelet
+ */
+define(['N/file', 'N/format', 'N/https', 'N/record', 'N/render', 'N/runtime'],
+    /**
+ * @param{file} file
+ * @param{format} format
+ * @param{https} https
+ * @param{record} record
+ * @param{render} render
+ * @param{runtime} runtime
+ */
+    (file, format, https, record, render, runtime) => {
+        /**
+         * Defines the Suitelet script trigger point.
+         * @param {Object} scriptContext
+         * @param {ServerRequest} scriptContext.request - Incoming request
+         * @param {ServerResponse} scriptContext.response - Suitelet response
+         * @since 2015.2
+         */
+        const onRequest = (scriptContext) => {
+
+        }
+
+        return {onRequest}
+
+    });
